cbee4140e0439a2ee1a5ccffe12fc7dd51a864aa
Andreas Haefner <andreas.haefner@axa-winterthur.ch>
2019-05-07 17:36:53 +0200

    Express Router

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
     http = require('http'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    bodyParser = require('body-parser')
+    dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,14 +10,27 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use('/dishes', dishRouter);
+  
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
 
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+app.post('/dishes/:dishId', (req, res, next) => {
+res.statusCode = 403;
+res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+res.write('Updating the dish: ' + req.params.dishId + '\n');
+res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
 });
 
 const server = http.createServer(app);
