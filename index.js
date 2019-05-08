c1ebcbd1aa38397808c3afaf4fe28831acb55381
haebri <andreas.haefner@gmx.net>
2019-05-08 20:58:49 +0200

    promotions REST API

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express'),
     http = require('http'),
     morgan = require('morgan'),
-    bodyParser = require('body-parser')
-    dishRouter = require('./routes/dishRouter');
+    bodyParser = require('body-parser'),
+    dishRouter = require('./routes/dishRouter'),
+    promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,10 +14,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
  
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
---
+++ routes/promoRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get('/', (req, res) => {
+    res.end('Will send all the promotions to you!');
+})
+.post('/', (req, res) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put('/', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete('/', (req, res) => {
+    res.end('Deleting all promotions');
+})
+.get('/:promotionId', (req,res) => {
+    res.end('Will send details of the promotion: ' + req.params.promotionId +' to you!');
+})
+.post('/:promotionId', (req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions/'+ req.params.promotionId);
+})
+.put('/:promotionId', (req, res) => {
+    res.write('Updating the promotion: ' + req.params.promotionId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+.delete('/:promotionId', (req, res) => {
+    res.end('Deleting promotion: ' + req.params.promotionId);
+});
+module.exports = router;
